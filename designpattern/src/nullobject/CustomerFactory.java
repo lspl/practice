38cda7af95dcfc8a6219a64b0c7a993d02594d5a
lspl <498264111@qq.com>
2017-07-17 16:59:06 +0800

    null object pattern

---
+++ designpattern/src/nullobject/CustomerFactory.java
@@ -0,0 +1,18 @@
+package nullobject;
+
+/**
+ * @author lishunpu
+ * @create 2017-07-17 16:36
+ */
+public class CustomerFactory {
+    public static final String[] names = {"Rob", "Joe", "Julie"};
+
+    public static AbstractCustomer getCustomer(String name) {
+        for (int i = 0; i < names.length; i++) {
+            if (names[i].equalsIgnoreCase(name)) {
+                return new RealCustomer(name);
+            }
+        }
+        return new NullCustomer();
+    }
+}
