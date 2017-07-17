7d113d75e4c146e8bb2e25efa6b0f0c26672bbfa
lspl <498264111@qq.com>
2017-07-17 14:41:28 +0800

    iterator pattern

---
+++ designpattern/src/iterator/Container.java
@@ -0,0 +1,9 @@
+package iterator;
+
+/**
+ * @author lishunpu
+ * @create 2017-07-17 14:31
+ */
+public interface Container {
+    public Iterator getIterator();
+}
---
+++ designpattern/src/iterator/Iterator.java
@@ -0,0 +1,10 @@
+package iterator;
+
+/**
+ * @author lishunpu
+ * @create 2017-07-17 14:30
+ */
+public interface Iterator {
+    public boolean hasNext();
+    public Object next();
+}
